b533edc61485d19ed9910a6cfbb05c0d2c9c53f3
Richard Sun <rsun@media.ucla.edu>
2016-03-05 16:24:33 -0800

    getoptlong structure

---
+++ Makefile
@@ -0,0 +1,34 @@
+CC = gcc
+CFLAGS = -std=gnu99 -Wall -Wextra -Wno-unused-parameter
+OPTIMIZE = -g# -O2
+
+SRC_DIR = src
+OBJ_DIR = obj
+SOURCES = $(wildcard $(SRC_DIR)/*.c)
+HEADERS = $(wildcard $(SRC_DIR)/*.h)
+OBJECTS = $(SOURCES:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+
+all: addtest
+
+addtest: $(OBJ_DIR) $(OBJECTS)
+	$(CC) $(CFLAGS) $(OPTIMIZE) -o $@ $(OBJECTS)
+
+$(OBJ_DIR):
+	mkdir -p $@
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c $(HEADERS)
+	$(CC) $(CFLAGS) $(OPTIMIZE) -c $< -o $@
+
+DISTDIR = lab1-michaelli
+DIST_FILES = Makefile README.md $(SRC_DIR)
+
+dist: $(DISTDIR)
+
+$(DISTDIR): clean $(DIST_FILES)
+	tar cf - --transform='s|^|$(DISTDIR)/|' $(DIST_FILES) | gzip -9 > $@.tar.gz
+
+
+clean:
+	rm -rf addtest obj/ *.o *.tmp $(DISTDIR) $(DISTDIR).tar.gz
+
+.PHONY: all check dist clean test
---
+++ src/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+enum {
+	THREADS = 1,
+	ITERATIONS,
+	LISTS,
+	SYNC
+};
+
+static struct option long_options[] =
+{
+	{"threads", required_argument, 0, THREADS},
+	{"iterations", required_argument, 0, ITERATIONS},
+	{"lists", required_argument, 0, LISTS},
+	{"sync", required_argument, 0, SYNC},
+	{0, 0, 0, 0}
+};
+
+/* getopt_long stores the option index here. */
+int option_index = 0;
+
+int main (int argc, char **argv) {
+
+	int c;
+	while (1)
+	{
+		c = getopt_long(argc, argv, "", long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1)
+			break;
+
+		switch (c)
+		{
+			case THREADS:
+				break;
+
+			case ITERATIONS:
+				break;
+
+			case LISTS:
+				break;
+
+			case SYNC:
+				break;
+
+			default:
+				/* code for unrecognized options */
+				break;
+		}
+	}
+
+	return 0;
+}
