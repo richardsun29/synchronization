7eae6381bab7ca136210952571ef1980e7ffa072
Michael Li <michael.li11702@gmail.com>
2016-03-11 16:46:26 -0800

    Set decimal precision for printing floats

--- sltest.c
+++ sltest.c
@@ -267,7 +267,7 @@ int main (int argc, char **argv) {
 	int sorted_list_size = length_func(sorted_lists[0]);
 	float length_per_thread = (float)num_iterations / num_lists;
 	float operations = num_threads * num_iterations * length_per_thread;
-	printf("%lld threads x %lld iterations x (ins + lookup/del) x (%f/2 avg len) = %f operations\n",
+	printf("%lld threads x %lld iterations x (ins + lookup/del) x (%.2f/2 avg len) = %.2f operations\n",
 		num_threads, num_iterations, length_per_thread, operations);
 
 	int size_err = sorted_list_size != 0;
@@ -278,7 +278,7 @@ int main (int argc, char **argv) {
 	long long elapsed = (end_time.tv_sec * pow(10, 9) + end_time.tv_nsec)
 			- (start_time.tv_sec * pow(10, 9) + start_time.tv_nsec);
 	printf("elapsed time: %lld ns\n", elapsed);
-	printf("per operation: %f ns\n", elapsed / operations);
+	printf("per operation: %.2f ns\n", elapsed / operations);
 
 	for (sl = 0; sl < num_lists; sl++) {
 		SortedList_free(sorted_lists[sl]);
